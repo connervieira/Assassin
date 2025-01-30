2fc2dfeb4a93bc2d013052d2fd9540b191854568
Conner Vieira <cvieira@v0lttech.com>
2025-01-30 16:20:33 -0500

    Tested Predator license plate count

--- main.py
+++ main.py
@@ -539,7 +539,7 @@ while True: # Run forever in a loop until terminated.
             print("Attention: " + str(datetime.timedelta(seconds=round(attention.get_current_attention_time()[0]))) + " active (" + str(datetime.timedelta(seconds=round(attention.get_current_attention_time()[1]))) + " reset)") # Print the current active attention time to the console.
 
         if (config["general"]["predator_integration"]["enabled"] == True and config["display"]["displays"]["predator"] == True): # Check to see if the Predator display is enabled.
-            print("Predator: " + str(len(predator_plate_history))) # Print the number of license plates currently seen by Predator. TODO: Test
+            print("Predator: " + str(len(predator_plate_history))) # Print the number of license plates currently seen by Predator.
 
         if (config["general"]["bluetooth_scanning"]["enabled"] == True and config["display"]["displays"]["bluetooth"] == True): # Check to see if the Bluetooth display is enabled.
             print("Bluetooth: " + str(len(bluetooth_devices))) # Print the number of nearby Bluetooth devices.
